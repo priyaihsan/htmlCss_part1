cb164d33dbf75044d23c332b10f69fe162544068
priyaihsan <priyaihsan771@gmail.com>
2021-04-18 04:14:33 +0700

    update all brand new

--- website.js
+++ website.js
@@ -1,5 +1,12 @@
-var myCarousel = document.querySelector('#myCarousel')
+// Fungsi AJAX Jquery untuk meload isi file
+
+$(function () {
+	$("#about1").load("text/about1.txt");
+	$("#about2").load("text/about2.txt");
+});
+
+var myCarousel = document.querySelector("#myCarousel");
 var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 1000,
-  wrap: false
-})
+	interval: 1000,
+	wrap: false,
+});
